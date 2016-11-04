b9151dbbf173c9ebcc23d21526326d65165952be
Christiaan van Zyl <christiaanvanzyl@gmail.com>
2016-11-04 14:27:43 +0200

    Does this isAdmin function work as expected?

--- app.go
+++ app.go
@@ -2,45 +2,59 @@ package main
 
 import (
 	"fmt"
-	"os/exec"
 	"crypto/md5"
 	"time"
 	"os"
-	"path"
 )
 
-func getWinDir() string {
-	out, err := exec.Command("echo %WINDIR%").Output()
-	if err != nil {
-		fmt.Println(fmt.Sprintf("%v", out))
-		fmt.Println(err)
-	}
-	return string(out)
-}
+const winDir = "c:\\Windows"
 
+// isAdmin tries to create a new file in the windows dir, if the file is created
+// then assume the program is running as administrator and return true
 func isAdmin() bool {
-	// File name is a hash of the current datetime string
+	 //File name is a hash of the current datetime string
 	t := time.Now()
 	data := []byte(t.Format(time.RFC3339))
-	file := fmt.Sprintf("%s.txt",
+	filePath := fmt.Sprintf("%s\\%s.txt", winDir,
 		fmt.Sprintf("%x", md5.Sum(data)))
-	filePath := path.Join(getWinDir(), file)
 
-	// If we can open a file for read / write in the system folder
-	// then assume that the app is running as administrator
-	f, err := os.OpenFile(filePath, os.O_RDWR, 0666)
-	defer f.Close()
-	if err != nil {
-		return false
+	fmt.Println(filePath)
+	if _, err := os.Stat(filePath); os.IsNotExist(err) {
+		// filePath does not exist
+		fmt.Println("not exist")
+
+		// If we can open a file for read / write in the system folder
+		// then assume that the app is running as administrator
+		f, err := os.OpenFile(filePath, os.O_RDWR|os.O_CREATE, 0666)
+		if err != nil {
+			fmt.Println(err)
+			fmt.Println("false")
+			return false
+		}
+
+		f.Close()
+		err = os.Remove(filePath)
+		if err != nil {
+			fmt.Println(err)
+			return false
+		}
+
+		fmt.Println("true")
+		return true
+
+	} else {
+		// File already exists, try a different filePath
+		fmt.Println("exists")
+		return isAdmin()
 	}
-	return true
 }
 
 func main() {
-	if !isAdmin() {
+	if isAdmin() {
+		fmt.Println("Running as administrator")
+	} else {
 		fmt.Println("Not running as administrator")
 	}
-	fmt.Println("Running as administrator")
 	fmt.Println("")
 	fmt.Println("Press any key to continue")
     fmt.Scanln()
